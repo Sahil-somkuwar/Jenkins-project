24b65c3f9ad9b08677875ee5b3eb58dbc8dc2292
SAHIL SOMKUWAR <sahilsomkuwar10@gmail.com>
2026-01-03 00:25:15 +0530

    fix

---
+++ var.tf
@@ -0,0 +1,21 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "cluster_name" {
+  default = "demo-eks-cluster1"
+}
+
+variable "node_instance_type" {
+  default = "m7i-flex.large"
+}
+
+variable "desired_nodes" {
+  default = 2
+}
+
+variable "key_pair_name" {
+  description = "EC2 Key Pair name for SSH access to nodes (optional)"
+  type        = string
+  default     = ""
+}
