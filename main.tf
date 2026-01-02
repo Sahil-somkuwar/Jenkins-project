cb5180e0d760f9e0fe5571f843e1ce3022c9c1b2
SAHIL SOMKUWAR <sahilsomkuwar10@gmail.com>
2026-01-02 22:50:23 +0530

    fix

--- main.tf
+++ main.tf
@@ -1,103 +1,125 @@
-provider "aws" { 
-
-  region = "ap-south-1"
+provider "aws" {
+  region = var.aws_region
 }
 
-resource "aws_iam_role" "eks_role" {
-  name = "eks-terraform-sahil"
+### Get Default VPC Subnets
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
-  role       = aws_iam_role.eks_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-data "aws_vpc" "default_vpc" {
+data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnets" "default_subnets" {
+data "aws_subnets" "default" {
   filter {
     name   = "vpc-id"
-    values = [data.aws_vpc.default_vpc.id]
+    values = [data.aws_vpc.default.id]
   }
 }
 
+### IAM Role for EKS Cluster
 
-resource "aws_eks_cluster" "eks_cluster" {
-  name     = "my-cluster"
-  role_arn = aws_iam_role.eks_role.arn
-  version  = "1.34"
-
-  access_config {
-    authentication_mode = "API"
-  }
-
-  vpc_config {
-    subnet_ids = data.aws_subnets.default_subnets.ids
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy
-  ]
-}
-
-
-resource "aws_iam_role" "eks_node_role" {
-  name = "eks-node-role-terraform-sahil"
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "eks.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "cluster_node_CNI_Policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "cluster_node_container_Policy" {
-  role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+resource "aws_iam_role_policy_attachment" "eks_vpc_resource_controller" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
 }
 
-resource "aws_iam_role_policy_attachment" "cluster_node_workernode_Policy" {
-  role       = aws_iam_role.eks_node_role.name
+
+### EKS Cluster
+
+resource "aws_eks_cluster" "eks" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+  version  = "1.28"
+
+  vpc_config {
+    subnet_ids              = data.aws_subnets.default.ids
+    endpoint_private_access = true
+    endpoint_public_access  = true
+    public_access_cidrs     = ["0.0.0.0/0"]
+  }
+
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy,
+    aws_iam_role_policy_attachment.eks_vpc_resource_controller,
+    aws_cloudwatch_log_group.eks_cluster
+  ]
+
+  tags = {
+    Name = var.cluster_name
+  }
+}
+
+### CloudWatch Log Group for EKS
+
+resource "aws_cloudwatch_log_group" "eks_cluster" {
+  name              = "/aws/eks/${var.cluster_name}/cluster"
+  retention_in_days = 7
+}
+
+### IAM Role for Worker Nodes
+
+
+resource "aws_iam_role" "node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_policy" {
+  role       = aws_iam_role.node_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
 
-resource "aws_eks_node_group" "eks_node_group" {
-  cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "eks-node-group"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
-  instance_types =  ["c7i-flex.large"]
-  subnet_ids = data.aws_subnets.default_subnets.ids
+resource "aws_iam_role_policy_attachment" "ecr_readonly_policy" {
+  role       = aws_iam_role.node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+### Node Group
+
+resource "aws_eks_node_group" "nodes" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "demo-node-group"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = data.aws_subnets.default.ids
+  instance_types  = [var.node_instance_type]
+  ami_type        = "AL2_x86_64"
+  disk_size       = 20
+  capacity_type   = "ON_DEMAND"
 
   scaling_config {
-    desired_size = 2
+    desired_size = var.desired_nodes
     max_size     = 3
     min_size     = 1
   }
@@ -106,9 +128,25 @@ resource "aws_eks_node_group" "eks_node_group" {
     max_unavailable = 1
   }
 
+  labels = {
+    role = "general"
+  }
+
+  dynamic "remote_access" {
+    for_each = var.key_pair_name != "" ? [1] : []
+    content {
+      ec2_ssh_key = var.key_pair_name
+    }
+  }
+
   depends_on = [
-    aws_iam_role_policy_attachment.cluster_node_CNI_Policy,
-    aws_iam_role_policy_attachment.cluster_node_container_Policy,
-    aws_iam_role_policy_attachment.cluster_node_workernode_Policy,
+    aws_eks_cluster.eks,
+    aws_iam_role_policy_attachment.node_policy,
+    aws_iam_role_policy_attachment.cni_policy,
+    aws_iam_role_policy_attachment.ecr_readonly_policy
   ]
+
+  tags = {
+    Name = "${var.cluster_name}-node-group"
+  }
 }
